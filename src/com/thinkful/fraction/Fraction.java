86e5d89db3c9a3de8af1cccfc59a98add69cff89
marouanekhabbaz <marwane.khab@gmail.com>
2021-11-10 15:15:59 -0500

    first commit

---
+++ src/com/thinkful/fraction/Fraction.java
@@ -0,0 +1,83 @@
+package com.thinkful.fraction;
+
+import java.util.Objects;
+
+public class Fraction {
+    private int numerator ;
+    private  int denominator ;
+
+    public Fraction(){
+        this(1, 1);
+    }
+
+    public   Fraction( int numerator , int denominator){
+        if(denominator == 0){
+            try {
+                throw new Exception("denominator should be different than 0");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        if(denominator == 0){
+            try {
+                throw new Exception("denominator should be different than 0");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.denominator = denominator;
+    }
+
+    @Override
+    public String toString() {
+        return numerator  + "/"+
+               + denominator ;
+    }
+
+    public Fraction add( Fraction fraction){
+
+        int communDenominator;
+        int communNumerator;
+        if(denominator == fraction.denominator){
+            communDenominator = denominator;
+            communNumerator = this.numerator + fraction.getNumerator();
+        }else{
+            communDenominator = this.denominator * fraction.getDenominator();
+            communNumerator = this.numerator * fraction.getDenominator() + fraction.getNumerator() * this.denominator;
+        }
+
+
+
+        return new Fraction(communNumerator, communDenominator);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return numerator == fraction.numerator && denominator == fraction.denominator;
+    }
+
+
+}
---
+++ src/com/thinkful/fraction/Main.java
@@ -0,0 +1,14 @@
+package com.thinkful.fraction;
+
+import java.util.Scanner;
+
+public class Main {
+    public static  void main (String[] args){
+     Fraction fraction1 = new Fraction(1,4);
+
+     Fraction fraction2 = new Fraction(2,5);
+
+        System.out.println(fraction1.add(fraction2));
+
+    }
+}
